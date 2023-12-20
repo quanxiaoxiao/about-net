149da0ad8e6e24a251aab3e9961e6ba68e3fef22
quan <quan_xiaoxiao@qq.com>
2023-12-20 13:41:01 +0800

    xxx

--- src/http/request.mjs
+++ src/http/request.mjs
@@ -1,3 +1,4 @@
+/* eslint no-use-before-define: 0 */
 import createConnector from '../createConnector.mjs';
 import getCurrentDateTime from '../getCurrentDateTime.mjs';
 import encodeHttp from './encodeHttp.mjs';
@@ -55,116 +56,192 @@ export default (
         state.connector.write(chunk);
       } catch (error) {
         emitError(error);
+        closeRequestStream();
       }
     };
 
-    state.connector = createConnector({
-      onConnect: async () => {
-        if (state.isActive) {
-          state.dateTimeConnect = getCurrentDateTime();
-          if (onRequest) {
-            try {
-              await onRequest(requestOptions);
-            } catch (error) {
-              state.connector();
-              emitError(error);
+    function handleDataOnRequestBody(chunk) {
+      if (state.isActive) {
+        try {
+          const b = state.encodeRequest(chunk);
+          if (b && b.length > 0) {
+            const ret = state.connector.write(b);
+            if (!ret) {
+              requestOptions.body.pause();
             }
           }
+        } catch (error) {
+          emitError(error);
+          requestOptions.body.off('data', handleDataOnRequestBody);
+          requestOptions.body.destroy();
+        }
+      } else {
+        requestOptions.body.off('data', handleDataOnRequestBody);
+        closeRequestStream();
+      }
+    }
+
+    function handleErrorOnRequestBody(error) {
+      emitError(error);
+    }
+
+    function handleEndOnRequestBody() {
+      requestOptions.body.off('close', handleCloseOnRequestBody);
+      if (state.isActive) {
+        try {
+          const ret = state.encodeRequest();
+          if (ret && ret.length > 0) {
+            state.connector.write(ret);
+          }
+        } catch (error) {
+          emitError(error);
+        }
+      }
+    }
+
+    function handleCloseOnRequestBody() {
+      requestOptions.body.off('end', handleEndOnRequestBody);
+      emitError(new Error('request body stream close'));
+    }
+
+    function closeRequestStream() {
+      if (requestOptions.body
+        && requestOptions.body.pipe
+        && !requestOptions.body.destroyed
+      ) {
+        requestOptions.body.destroy();
+      }
+    }
+
+    state.connector = createConnector(
+      {
+        onConnect: async () => {
           if (state.isActive) {
-            if (requestOptions.body && requestOptions.body.pipe) {
-              state.encodeRequest = encodeHttp({
-                path: requestOptions.path,
-                method: requestOptions.method,
-                headers: requestOptions.headers,
-                onHeader: (chunkRequestHeader) => {
-                  state.dateTimeRequestSend = getCurrentDateTime();
-                  outgoing(Buffer.concat([
-                    chunkRequestHeader,
-                    Buffer.from('\r\n'),
-                  ]));
+            state.dateTimeConnect = getCurrentDateTime();
+            if (onRequest) {
+              try {
+                await onRequest(requestOptions);
+              } catch (error) {
+                state.connector();
+                closeRequestStream();
+                emitError(error);
+              }
+            }
+            if (state.isActive) {
+              if (requestOptions.body && requestOptions.body.pipe) {
+                if (!requestOptions.body.readable) {
+                  emitError(new Error('request body stream unable read'));
+                } else {
+                  state.encodeRequest = encodeHttp({
+                    path: requestOptions.path,
+                    method: requestOptions.method,
+                    headers: requestOptions.headers,
+                  });
+                  requestOptions.body.once('error', handleErrorOnRequestBody);
+                  requestOptions.body.once('close', handleCloseOnRequestBody);
+                  requestOptions.body.once('end', handleEndOnRequestBody);
+                  requestOptions.body.on('data', handleDataOnRequestBody);
+                }
+              } else {
+                state.dateTimeRequestSend = getCurrentDateTime();
+                outgoing(encodeHttp(requestOptions));
+              }
+            }
+          } else {
+            state.connector();
+          }
+        },
+        onDrain: () => {
+          if (state.isActive
+            && requestOptions.body
+            && requestOptions.body.pipe
+            && requestOptions.body.isPaused()
+          ) {
+            requestOptions.body.resume();
+          }
+        },
+        onData: async (chunk) => {
+          if (state.isActive) {
+            if (!state.decode) {
+              state.dateTimeResponse = getCurrentDateTime();
+              if (onResponse) {
+                await onResponse();
+              }
+              state.decode = decodeHttpResponse({
+                onStartLine: (ret) => {
+                  state.statusCode = ret.statusCode;
+                  state.httpVersion = ret.httpVersion;
+                  state.statusText = ret.statusText;
+                },
+                onHeader: (ret) => {
+                  state.dateTimeHeader = getCurrentDateTime();
+                  state.headers = ret.headers;
+                  state.headersRaw = ret.headersRaw;
+                },
+                onBody: (bodyChunk) => {
+                  if (state.dateTimeBody == null) {
+                    state.dateTimeBody = getCurrentDateTime();
+                  }
+                  if (bodyChunk && bodyChunk.length > 0) {
+                    state.bodyBufList.push(bodyChunk);
+                  }
+                },
+                onEnd: () => {
+                  state.dateTimeEnd = getCurrentDateTime();
+                  if (state.dateTimeBody == null) {
+                    state.dateTimeBody = state.dateTimeEnd;
+                  }
+                  if (state.isActive) {
+                    state.isActive = false;
+                    state.connector.end();
+                    resolve({
+                      dateTimeCreate: state.dateTimeCreate,
+                      dateTimeConnect: state.dateTimeConnect,
+                      dateTimeResponse: state.dateTimeResponse,
+                      dateTimeHeader: state.dateTimeHeader,
+                      dateTimeBody: state.dateTimeBody,
+                      dateTimeEnd: state.dateTimeEnd,
+                      dateTimeRequestSend: state.dateTimeRequestSend,
+                      statusCode: state.statusCode,
+                      httpVersion: state.httpVersion,
+                      statusText: state.statusText,
+                      headers: state.headers,
+                      headersRaw: state.headersRaw,
+                      body: Buffer.concat(state.bodyBufList),
+                    });
+                  }
                 },
               });
-            } else {
-              state.dateTimeRequestSend = getCurrentDateTime();
-              outgoing(encodeHttp(requestOptions));
             }
-          }
-        } else {
-          state.connector();
-        }
-      },
-      onData: async (chunk) => {
-        if (state.isActive) {
-          if (!state.decode) {
-            state.dateTimeResponse = getCurrentDateTime();
-            if (onResponse) {
-              await onResponse();
+            if (onChunk) {
+              await onChunk(chunk);
             }
-            state.decode = decodeHttpResponse({
-              onStartLine: (ret) => {
-                state.statusCode = ret.statusCode;
-                state.httpVersion = ret.httpVersion;
-                state.statusText = ret.statusText;
-              },
-              onHeader: (ret) => {
-                state.dateTimeHeader = getCurrentDateTime();
-                state.headers = ret.headers;
-                state.headersRaw = ret.headersRaw;
-              },
-              onBody: (bodyChunk) => {
-                if (state.dateTimeBody == null) {
-                  state.dateTimeBody = getCurrentDateTime();
-                }
-                if (bodyChunk && bodyChunk.length > 0) {
-                  state.bodyBufList.push(bodyChunk);
-                }
-              },
-              onEnd: () => {
-                state.dateTimeEnd = getCurrentDateTime();
-                if (state.dateTimeBody == null) {
-                  state.dateTimeBody = state.dateTimeEnd;
-                }
-                if (state.isActive) {
-                  state.isActive = false;
-                  state.connector.end();
-                  resolve({
-                    dateTimeCreate: state.dateTimeCreate,
-                    dateTimeConnect: state.dateTimeConnect,
-                    dateTimeResponse: state.dateTimeResponse,
-                    dateTimeHeader: state.dateTimeHeader,
-                    dateTimeBody: state.dateTimeBody,
-                    dateTimeEnd: state.dateTimeEnd,
-                    dateTimeRequestSend: state.dateTimeRequestSend,
-                    statusCode: state.statusCode,
-                    httpVersion: state.httpVersion,
-                    statusText: state.statusText,
-                    headers: state.headers,
-                    headersRaw: state.headersRaw,
-                    body: Buffer.concat(state.bodyBufList),
-                  });
-                }
-              },
-            });
-          }
-          if (onChunk) {
-            await onChunk(chunk);
-          }
-          try {
-            await state.decode(chunk);
-          } catch (error) {
+            try {
+              await state.decode(chunk);
+            } catch (error) {
+              state.connector();
+              closeRequestStream();
+              emitError(error);
+            }
+          } else {
             state.connector();
-            emitError(error);
           }
-        } else {
-          state.connector();
-        }
+        },
+        onError: (error) => {
+          emitError(error);
+          closeRequestStream();
+        },
+        onClose: () => {
+          emitError(new Error('socket is close'));
+          closeRequestStream();
+        },
       },
-      onError: emitError,
-      onClose: () => emitError(new Error('socket is close')),
-    }, getConnect);
+      getConnect,
+    );
 
     if (!state.connector) {
       emitError(new Error('create connector fail'));
+      closeRequestStream();
     }
   });
 };
