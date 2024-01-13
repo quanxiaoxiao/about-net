8f946fc6d0196c299b4ade0db4a57893bc614613
quan <quan_xiaoxiao@qq.com>
2024-01-13 15:39:37 +0800

    xx

--- src/dns/resolveIPv4.mjs
+++ src/dns/resolveIPv4.mjs
@@ -1,18 +1,15 @@
-import net from 'node:net';
 import tls from 'node:tls';
+import net from 'node:net';
 import enpack from '../enpack.mjs';
 import depack from '../depack.mjs';
-import createTlsConnector from '../createTlsConnector.mjs';
+import createConnector from '../createConnector.mjs';
 import decodeAnswer from './decodeAnswer.mjs';
 import encodeV4 from './encodeV4.mjs';
 import { RECORD_TYPE_A } from './recordTypes.mjs';
 
 export default (hostname, server, port) => {
-  if (net.isIPv4(hostname)) {
+  if (net.isIPv4(hostname) || net.isIPv6(hostname)) {
     return [hostname];
-  }
-  if (net.isIPv6(hostname)) {
-    throw new Error(`\`${hostname}\` invalid`);
   }
   const state = {
     depack: depack(),
@@ -20,67 +17,67 @@ export default (hostname, server, port) => {
     complete: false,
     tick: null,
   };
+
   return new Promise((resolve, reject) => {
-    const connector = createTlsConnector({
-      hostname: server || process.env.DNS || '223.5.5.5',
-      port: port || 853,
-      onError: (error) => {
-        if (!state.complete) {
-          state.complete = true;
+    const emitError = (error) => {
+      if (!state.complete) {
+        state.complete = true;
+        if (state.tick != null) {
           clearTimeout(state.tick);
-          reject(error);
+          state.tick = null;
         }
-      },
-      onClose: () => {
-        if (!state.complete) {
-          state.complete = true;
-          clearTimeout(state.tick);
-          reject(new Error('socket close'));
-        }
-      },
-      onData: (chunk) => {
-        if (!state.complete) {
-          const depackRet = state.depack(chunk);
-          if (depackRet) {
-            try {
-              const { answers } = state.decode(depackRet.payload);
-              const addresses = answers.filter((d) => d.recordType === RECORD_TYPE_A);
-              state.complete = true;
-              if (addresses.length === 0) {
-                reject(new Error(`\`${hostname}\` lookup address is empty`));
-              } else {
-                resolve(addresses.map((d) => d.address));
-              }
-              clearTimeout(state.tick);
-              connector.end();
-            } catch (error) {
-              if (!state.complete) {
+        reject(error);
+      }
+    };
+    const connector = createConnector(
+      {
+        onError: emitError,
+        onClose: () => {
+          emitError(new Error('socket close'));
+        },
+        onData: (chunk) => {
+          if (!state.complete) {
+            const depackRet = state.depack(chunk);
+            if (depackRet) {
+              try {
+                const { answers } = state.decode(depackRet.payload);
+                const addresses = answers.filter((d) => d.recordType === RECORD_TYPE_A);
+                if (addresses.length === 0) {
+                  throw new Error(`\`${hostname}\` lookup address is empty`);
+                }
+                if (state.tick != null) {
+                  clearTimeout(state.tick);
+                  state.tick = null;
+                }
                 state.complete = true;
-                clearTimeout(state.tick);
-                reject(error);
+                resolve(addresses.map((d) => d.address));
+                connector.end();
+              } catch (error) {
+                emitError(error);
+                connector();
               }
-              connector();
             }
+          } else {
+            connector();
           }
-        } else {
-          connector();
-        }
+        },
       },
-      secureContext: tls.createSecureContext({
-        secureProtocol: 'TLSv1_2_method',
+      () => tls.connect({
+        host: server || process.env.DNS || '223.5.5.5',
+        port: port || 853,
+        secureContext: tls.createSecureContext({
+          secureProtocol: 'TLSv1_2_method',
+        }),
       }),
-    });
+    );
 
     connector.write(enpack(encodeV4({
       hostname,
     })));
 
     state.tick = setTimeout(() => {
-      if (!state.complete) {
-        state.complete = true;
-        connector();
-        reject(new Error('request timeout'));
-      }
+      state.tick = null;
+      emitError(new Error('request timeout'));
     }, 10 * 1000);
   });
 };
